197f8c531f71047cb72620d561ba08ed2c337f35
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2025-09-07 21:36:52 +0900

    setting: Xcode 프로젝트 생성
    1. Bundle ID 설정
    2. iOS Minimum Deployments 15.0 설정

---
+++ Algohub-iOS/Algohub-iOSTests/Algohub_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Algohub_iOSTests.swift
+//  Algohub-iOSTests
+//
+//  Created by Jae Hyun Lee on 9/7/25.
+//
+
+import Testing
+@testable import Algohub_iOS
+
+struct Algohub_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
